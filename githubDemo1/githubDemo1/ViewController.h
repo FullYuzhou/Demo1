5a91005efec0268715c1ab0a343a30ec002f07de
FullYuzhou <1032375314@qq.com>
2015-05-20 14:58:17 +0800

    321

---
+++ githubDemo1/githubDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubDemo1
+//
+//  Created by Yuzhou on 15/5/20.
+//  Copyright (c) 2015年 周宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ githubDemo1/githubDemo1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  githubDemo1
+//
+//  Created by Yuzhou on 15/5/20.
+//  Copyright (c) 2015年 周宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
